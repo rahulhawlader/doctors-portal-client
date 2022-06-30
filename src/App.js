d99f0c7c5a1cf11895a7482b720fe06691b0a6a0
Rahulhawlader <hawladerrahul8@gmail.com>
2022-06-30 09:46:13 +0600

    navber componets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import Navber from './Pages/Shared/Navber/Navber';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/Home/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navber></Navber>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+
+      </Routes>
+
+
     </div>
   );
 }
